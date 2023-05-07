521665b8794d2f852e0699229d0b868f8b417659
ttapu <kkurtisz@gmail.com>
2023-05-07 12:19:48 +0200

    clear

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { useContext } from "react";
 import { Route, Routes } from "react-router-dom"
 import Patterns from "./pages/Patterns"
 import './App.scss'
 import Board from './pages/Board';
-import { BoardContext } from "./BoardContext.ts";
 import { BoardProvider } from './BoardContext.tsx';
 
 
@@ -12,10 +10,10 @@ const App = () => {
   return (
     <>
       <BoardProvider>
-          <Routes>
-            <Route path="/" element={<Board />} />
-            <Route path="/patterns" element={<Patterns />} />
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Board />} />
+          <Route path="/patterns" element={<Patterns />} />
+        </Routes>
       </BoardProvider>
     </>
   )
