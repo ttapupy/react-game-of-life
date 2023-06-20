5ad895e639d50addb7b7ad3f1b4cf7b31004f186
ttapu <kkurtisz@gmail.com>
2023-06-20 12:23:14 +0200

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom/client'
+import * as ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { HashRouter } from 'react-router-dom';
